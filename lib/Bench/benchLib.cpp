1d33d6cd0104056dd239b88797d7fa7221cc65b9
aruiz08 <111095677+aruiz08@users.noreply.github.com>
2022-09-06 18:37:05 -0500

    Delete benchLib, instead use Questions

--- lib/Bench/benchLib.cpp
+++
@@ -1,7 +0,0 @@
-#include "benchLib.h"
-
-void writeBenchQuestion(Question newQuestion, PublicUser admin){}
-
-void updateBenchQuestion(Question update, PublicUser admin){}
-
-void deleteBenchQuestion(Question deleted, PublicUser admin){}
