6451cac9b708e48d49a1ce7b017b4443ae0f32b8
Sebastian Galindo <sebastian.galindo143@gmail.com>
2022-09-17 13:40:26 -0500

    Implementando acciones de los examenes

--- lib/Exams/exam.cpp
+++ lib/Exams/exam.cpp
@@ -1,7 +1,147 @@
 #include "exam.h"
 
-void createExam(Exam newExam){}
+int createExam(Exam newExam){
+    ifstream file("..\\files\\exams.dat", ios::binary);
 
-void updateExam(char* id){}
+    /*
+     * exams.dat
+     *
+     * RegExam ----------------
+     * Questions --------------
+     *
+     * RegExam ---------------
+     * Questions -------------
+     *
+     * */
 
-void deleteExam(char* id){}
+    if (!file)
+        return 1;
+
+    while (!file.eof()) {
+        RegExam readExam;
+        if (file.read((char*)&readExam, sizeof(readExam))) {
+            if (strcmp(readExam.ID, newExam.ID) == 0) {
+                file.close();
+                return 2;
+            }
+            for (int i = 0; i < readExam.numberQuestions; i++) {
+                Question readQuestion;
+                file.read((char*)&readQuestion, sizeof(readQuestion));
+            }
+        }
+    }
+
+    file.close();
+
+    ofstream examFile("..\\files\\exams.dat", ios::binary | ios::app);
+
+    if (!examFile)
+        return 1;
+
+    RegExam writeExam;
+
+    strcpy(writeExam.ID, newExam.ID);
+    strcpy(writeExam.name, newExam.name);
+    writeExam.date = newExam.date;
+    strcpy(writeExam.owner, newExam.owner);
+    writeExam.numberQuestions = newExam.numberQuestions;
+    RegExam.Student = newExam.Student;
+
+    examFile.write((char*)&writeExam, sizeof(writeExam));
+
+    for (int i = 0; i < newExam.numberQuestions; i++) {
+        examFile.write((char*)(newExam.questions + i), sizeof(*(newExam.questions + i)));
+    }
+
+    examFile.close();
+
+    return 0;
+}
+
+int updateExam(char* id, Exam updateExam){
+    fstream file("..\\files\\exams.dat", ios::binary | ios::in | ios::out);
+
+    if (!file)
+        return 1;
+
+    bool found = false;
+
+    while (!file.eof() && !found) {
+        RegExam readExam;
+        if (file.read((char*)&readExam, sizeof(readExam))) {
+            if (strcmp(readExam.ID, id) == 0) {
+                found = true;
+                file.seekg((int) (-1) * sizeof(readExam), ios::cur);
+                RegExam writeExam;
+
+                strcpy(writeExam.ID, updateExam.ID);
+                strcpy(writeExam.name, updateExam.name);
+                writeExam.date = updateExam.date;
+                strcpy(writeExam.owner, updateExam.owner);
+                writeExam.numberQuestions = updateExam.numberQuestions;
+                RegExam.Student = updateExam.Student;
+
+                file.write((char *) &writeExam, sizeof(writeExam));
+
+                for (int i = 0; i < readExam.numberQuestions; i++) {
+                    file.write((char *) (updateExam.questions + i), sizeof(*(updateExam.questions + i)));
+                }
+
+            } else {
+                for (int i = 0; i < readExam.numberQuestions; i++) {
+                    Question readQuestion;
+                    file.read((char*)&readQuestion, sizeof(readQuestion));
+                }
+            }
+        }
+    }
+
+    if (!found)
+        return 2;
+
+    file.close();
+
+    return 0;
+}
+
+int deleteExam(char* id){
+    ifstream readFile("..\\files\\exams.dat", ios::binary);
+    ofstream writeFile("..\\files\\temp.dat", ios::binary);
+
+    if(!readFile || !writeFile)
+        return 1;
+
+    bool found = false;
+
+    while (!readFile.eof()) {
+        RegExam readExam;
+        if (readFile.read((char*)&readExam, sizeof(readExam))) {
+            if (strcmp(readExam.ID, id) == 0) {
+                found = true;
+                for (int i = 0; i < readExam.numberQuestions; i++) {
+                    Question readQuestion;
+                    readFile.read((char*)&readQuestion, sizeof(readQuestion));
+                }
+            } else {
+                writeFile.write((char*)&readExam, sizeof(readExam));
+                for (int i = 0; i < readExam.numberQuestions; i++) {
+                    Question readQuestion;
+                    readFile.read((char*)&readQuestion, sizeof(readQuestion));
+                    writeFile.write((char*)&readQuestion, sizeof(readQuestion));
+                }
+            }
+        }
+    }
+
+    readFile.close();
+    writeFile.close();
+
+    remove("..\\files\\exams.dat");
+    rename("..\\files\\temp.dat", "..\\files\\exams.dat");
+
+    if (!found) {
+        return 2;
+    } else {
+        return 0;
+    }
+}
--- lib/Exams/exam.h
+++ lib/Exams/exam.h
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <chrono>
 #include <ctime>
+#include <fstream>
 #include "..\\Question\\question.h"
 
 struct listStudent {
@@ -22,12 +23,43 @@ struct Exam {
     Question *questions; //  Convertirlo en dinamico [3] min
     listStudent Student;
     char owner[40]; // admin username
+    int numberQuestions;
 };
 
-void createExam(Exam newExam);
+struct RegExam {
+    char ID[3];
+    char name[20];
+    time_t date;
+    listStudent Student;
+    char owner[40];
+    int numberQuestions;
+};
 
-void updateExam(char* id);
+/*
+ * Nombre: createExam
+ * Objetivo: Crear un examen
+ * Entradas: El objeto del examen que se va a crear
+ * Salidas: Un entero que representa el codigo de la transaccion
+ *          1 -> Fallo en los archivos, 2 -> Fallo en el proceso (Logica) y 0 -> Transaccion exitosa
+ * */
+int createExam(Exam newExam);
 
-void deleteExam(char* id);
+/*
+ * Nombre: updateExam
+ * Objetivo: Actualizar un examen
+ * Entradas: El ID del examen que se va a actualizar y el objeto del examen que se va a actualizar
+ * Salidas: Un entero que representa el codigo de la transaccion
+ *          1 -> Fallo en los archivos, 2 -> Fallo en el proceso (Logica) y 0 -> Transaccion exitosa
+ * */
+int updateExam(char* id, Exam updateExam);
+
+/*
+ * Nombre: deleteExam
+ * Objetivo: Eliminar un examen
+ * Entradas: El ID del examen que se va a eliminar
+ * Salidas: Un entero que representa el codigo de la transaccion
+ *          1 -> Fallo en los archivos, 2 -> Fallo en el proceso (Logica) y 0 -> Transaccion exitosa
+ * */
+int deleteExam(char* id);
 
 #endif //EXAM_H
